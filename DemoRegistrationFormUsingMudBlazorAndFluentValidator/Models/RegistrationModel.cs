c38f7828e5f912dd0b9bb459b07f577dde842f0a
Netcode-Hub <Rastinga1995@hotmail.com>
2023-08-24 03:01:48 +0000

    Add project files.

---
+++ DemoRegistrationFormUsingMudBlazorAndFluentValidator/Models/RegistrationModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DemoRegistrationFormUsingMudBlazorAndFluentValidator.Models
+{
+    public class RegistrationModel
+    {
+        public string Name { get; set; }
+        [EmailAddress]
+        public string Email { get; set; }
+       
+        public string Password { get; set; }
+        
+        public string ConfirmPassword { get; set; }
+        
+        public int PhoneNumber { get; set; }
+    }
+}
---
+++ DemoRegistrationFormUsingMudBlazorAndFluentValidator/Models/RegistrationModelValidation.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+namespace DemoRegistrationFormUsingMudBlazorAndFluentValidator.Models
+{
+    public class RegistrationModelValidation : AbstractValidator<RegistrationModel>
+    {
+        public RegistrationModelValidation()
+        {
+            RuleFor(x=> x.Name).NotEmpty();
+            RuleFor(x => x.Email).NotEmpty();
+            RuleFor(x => x.PhoneNumber).NotEmpty();
+            RuleFor(x => x.Password).NotEmpty().MinimumLength(5).MaximumLength(10).Matches(@"[A-Z]+").Matches(@"[a-z]+").Matches(@"[0-9]+").Matches(@"[\@\!\?\.\*]+");
+            RuleFor(x => x.ConfirmPassword).NotEmpty().Equal(_=> _.Password);
+            
+        }
+
+        public Func<object, string, Task<IEnumerable<string>>> ValidateValue => async (model, propertyName) =>
+        {
+            var result = await ValidateAsync(ValidationContext<RegistrationModel>.CreateWithOptions((RegistrationModel)model, x => x.IncludeProperties(propertyName)));
+            if (result.IsValid)
+                return Array.Empty<string>();
+            return result.Errors.Select(e => e.ErrorMessage);
+        };
+    }
+}
